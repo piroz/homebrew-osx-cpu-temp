0ebcafbf03ec291169f175ce11ce085d665d4d17
Hiroshi Murakami <piroz@users.noreply.github.com>
2017-03-17 15:58:10 +0900

    fix invalid formula `version (nil)`

--- osx-cpu-temp.rb
+++ osx-cpu-temp.rb
@@ -3,6 +3,7 @@ class OsxCpuTemp < Formula
   homepage "https://github.com/lavoiesl/osx-cpu-temp"
   url "https://codeload.github.com/lavoiesl/osx-cpu-temp/zip/master"
   sha256 "c1a142932c19b70fa1e2347204dbb8de1504bcd8c3eb357c772d8757f76dab84"
+  version "0.01"
 
   def install
     system "make"

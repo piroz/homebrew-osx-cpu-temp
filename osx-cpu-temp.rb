7e5b9b275452cceccd59765866a327c8d5d9508f
Hiroshi Murakami <piroz@users.noreply.github.com>
2017-10-25 18:30:22 +0900

    update sha256 checksum

--- osx-cpu-temp.rb
+++ osx-cpu-temp.rb
@@ -2,8 +2,8 @@ class OsxCpuTemp < Formula
   desc "Outputs current CPU temperature for OSX."
   homepage "https://github.com/lavoiesl/osx-cpu-temp"
   url "https://codeload.github.com/lavoiesl/osx-cpu-temp/zip/master"
-  sha256 "6905179cf73bc20858ba231e3eeb713b87916c5b012976403bd3733c1c487da6"
-  version "0.02"
+  sha256 "bad19be40f75363c057f4a5e5c225192a8e2eb6b7d17eb10cd5db4994caebf62"
+  version "0.03"
 
   def install
     system "make"
